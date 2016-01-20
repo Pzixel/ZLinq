d27e2dc262ee1b97358fe60d3fb5fc23adb29019
Pzixel <pzixel@gmail.com>
2016-01-20 11:31:01 +0300

    Initial structure

---
+++ ZLinq.TTHelp/TextTemplate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace ZLinq.TTHelp
+{
+    public class TextTemplate
+    {
+        public static readonly string[] NumberTypes =
+        {
+            "sbyte",
+            "byte",
+            "short",
+            "ushort",
+            "int",
+            "uint",
+            "long",
+            "ulong",
+            "float",
+            "double",
+            "decimal"
+        };
+
+        public static readonly string[] NullableNumberTypes = NumberTypes.Select(x => x + "?").ToArray();
+
+        public static readonly string[] CollectionsIList = { "T[]", "List<T>", "IList<T>" };
+        public static readonly string[] CollectionsIListInt = CollectionsIList.Select(x => x.Replace("T", "int")).ToArray();
+        public static readonly string[] EnumerablesList = CollectionsIList.Union(new[] { "ICollection<T>", "IEnumerable<T>" }).ToArray();
+        public static readonly string[] EnumerablesListInt = EnumerablesList.Select(x => x.Replace("T", "int")).ToArray();
+
+
+        public static string LengthOrCount(string x)
+        {
+            return x.IndexOf("[]", StringComparison.Ordinal) >= 0 ? "Length" : "Count";
+        }
+
+        public static string ExplicitCastFromIntIfNeeded(string typeName)
+        {
+            return Array.IndexOf(NumberTypes, typeName) < 4 ? "(" + typeName + ")" : "";
+        }
+
+        public static string GetCollectionName(string collection)
+        {
+            if (collection.IndexOf('[') >= 0)
+                return "Array";
+            return collection.Remove(collection.IndexOf('<'));
+        }
+
+        public static string ToArrayOrToList(string collection)
+        {
+            return GetCollectionName(collection) == "Array" ? "ToArray()" : "ToList()";
+        }
+
+        public static string IsNotNull(string paramName)
+        {
+            return string.Format("{0}.IsNotNull(\"{0}\");", paramName);
+        }
+    }
+}
