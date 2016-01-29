71091a0606a00c71e73c76fe50403238bd8e85be
Pzixel <pzixel@gmail.com>
2016-01-29 13:30:26 +0300

    Comparers added

---
+++ ZLinq.Test/Comparers/CustomComparerTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZLinq.Comparers;
+
+namespace ZLinq.Test.Comparers
+{
+    [TestClass]
+    public class CustomComparerTest
+    {
+        private class Test
+        {
+            public int A;
+            public double B;
+            public string C;
+            public byte D;
+        }
+
+        // For objects A, B and C, the following must be true:
+        // A.CompareTo(A) must return zero.
+        // If A.CompareTo(B) returns zero, then B.CompareTo(A) must return zero.
+        // If A.CompareTo(B) returns zero and B.CompareTo(C) returns zero, then A.CompareTo(C) must return zero.
+        // If A.CompareTo(B) returns a value other than zero, then B.CompareTo(A) must return a value of the opposite sign.
+        // If A.CompareTo(B) returns a value x not equal to zero, and B.CompareTo(C) returns a value y of the same sign as x, then A.CompareTo(C) must return a value of the same sign as x and y.
+        // https://msdn.microsoft.com/en-us/library/system.icomparable.compareto%28v=vs.110%29.aspx
+
+        [TestMethod]
+        public void TestComparerPartial()
+        {
+            var objA = new Test { A = 1, B = 19, C = "H", D = 10 };
+            var objB = new Test { A = 1, B = 20, C = "H", D = 0 };
+            var objC = new Test { A = 1, B = 0, C = "H", D = 20 };
+
+            var comparer = CustomComparer<Test>.New(t => t.A).Add(t => t.C).CreateDelegate();
+
+            Assert.AreEqual(0, comparer(objA, objB));
+            Assert.AreEqual(0, comparer(objB, objA));
+
+            Assert.AreEqual(0, comparer(objB, objC));
+            Assert.AreEqual(0, comparer(objA, objC));
+        }
+
+        [TestMethod]
+        public void TestComparerFull1()
+        {
+            var objA = new Test { A = 1, B = 19, C = "H", D = 10 };
+            var objB = new Test { A = 1, B = 19, C = "H", D = 10 };
+            var objC = new Test { A = 1, B = 19, C = "H", D = 10 };
+
+            var customComparer = CustomComparer<Test>.New(t => t.A).Add(t => t.B).Add(t => t.C).Add(t => t.D);
+            var comparer = customComparer.CreateDelegate();
+
+            Assert.AreEqual(0, comparer(objA, objB));
+            Assert.AreEqual(0, comparer(objB, objA));
+
+            Assert.AreEqual(0, comparer(objB, objC));
+            Assert.AreEqual(0, comparer(objA, objC));
+        }
+
+        [TestMethod]
+        public void TestComparerFull2()
+        {
+            var objA = new Test { A = 1, B = 20, C = "H", D = 0 };
+            var objB = new Test { A = 1, B = 20, C = "H", D = 10 };
+            var objC = new Test { A = 1, B = 30, C = "H", D = 10 };
+
+            var customComparer = CustomComparer<Test>.New(t => t.A).Add(t => t.B).Add(t => t.C).Add(t => t.D);
+            var compFull = customComparer.CreateDelegate();
+
+            Assert.IsTrue(compFull(objA, objB) < 0);
+            Assert.IsTrue(compFull(objB, objA) > 0);
+
+            Assert.IsTrue(compFull(objB, objC) < 0);
+            Assert.IsTrue(compFull(objA, objC) < 0);
+        }
+
+        [TestMethod]
+        public void TestComparerFull4()
+        {
+            Tuple<string, int> a = new Tuple<string, int>("1", 2), b = new Tuple<string, int>("2", 4);
+            Tuple<string, int> x = new Tuple<string, int>("2", 2), y = new Tuple<string, int>("2", 4);
+            var comparer = CustomComparer<Tuple<string, int>>.New(t => t.Item1).Add(t => t.Item2).CreateDelegate();
+            var comparerNeg = CustomComparer<Tuple<string, int>>.NewNeg(t => t.Item1).Add(t => -t.Item2).CreateDelegate();
+
+            Assert.AreEqual(-1, comparer(a, b));
+            Assert.AreEqual(1, comparer(b, a));
+
+            Assert.AreEqual(1, comparerNeg(x, y));
+            Assert.AreEqual(-1, comparerNeg(y, x));
+        }
+    }
+}
---
+++ ZLinq/Comparers/CustomComparer.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ZLinq.Comparers
+{
+    [SuppressMessage("ReSharper", "StaticMemberInGenericType")]
+    public sealed class CustomComparer<T>
+    {
+        private static readonly Dictionary<string, BlockExpression> MemberMemoizeDictionary = new Dictionary<string, BlockExpression>();
+        private static readonly LabelTarget RetLabelTarget = Expression.Label(typeof (int));
+
+        private static readonly ParameterExpression[] Parameters =
+        {
+            Expression.Parameter(typeof (T), "x"),
+            Expression.Parameter(typeof (T), "y")
+        };
+
+        private CustomComparer()
+        {
+
+        }
+
+        private BlockExpression Value { get; set; }
+
+        private static BlockExpression CreateBody<TProperty>(CustomComparer<T> comparer, Expression<Func<T, TProperty>> member, bool negate)
+            where TProperty : IComparable<TProperty>, IComparable
+        {
+            MemberExpression memberExpression = member.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                negate = true;
+                memberExpression = (MemberExpression) ((UnaryExpression) member.Body).Operand;
+            }
+            BlockExpression result = comparer.GetCompareTo<TProperty>(negate, memberExpression);
+            return result;
+        }
+
+        private BlockExpression GetCompareTo<TProperty>(bool negate, MemberExpression memberExpression) where TProperty : IComparable<TProperty>, IComparable
+        {
+            string name = memberExpression.Member.Name;
+            if (negate)
+                name += "Neg";
+            BlockExpression result;
+            if (MemberMemoizeDictionary.TryGetValue(name, out result))
+            {
+                return result;
+            }
+            result = CreateCompareTo<TProperty>(memberExpression, negate);
+            MemberMemoizeDictionary[name] = result;
+            return result;
+        }
+
+        private BlockExpression CreateCompareTo<TProperty>(MemberExpression memberExpression, bool negate) where TProperty : IComparable<TProperty>, IComparable
+        {
+            var compareTo = GetCompareToExpression<TProperty>(memberExpression);
+            var compareToVariable = compareTo.Key;
+            var compareToCall = compareTo.Value;
+            ConstantExpression zero = Expression.Constant(0, typeof (int));
+            return Expression.Block(new[] {compareToVariable},
+                Expression.Assign(compareToVariable, compareToCall),
+                Expression.IfThen(Expression.NotEqual(compareToVariable, zero),
+                    Expression.Return(RetLabelTarget, negate ? Expression.Negate(compareToVariable) : (Expression) compareToVariable)));
+        }
+
+
+        private static KeyValuePair<ParameterExpression, MethodCallExpression> GetCompareToExpression<TProperty>(MemberExpression memberExpression)
+            where TProperty : IComparable<TProperty>, IComparable
+        {
+            var member = memberExpression.Member;
+            var prop0 = Expression.PropertyOrField(Parameters[0], member.Name);
+            var prop1 = Expression.PropertyOrField(Parameters[1], member.Name);
+            var compareTo = GetCompareToMethodInfo<TProperty>();
+            var compareToCall = Expression.Call(prop0, compareTo, prop1);
+            var parameterExpression = Expression.Variable(typeof (int), "compare" + member.Name);
+            return new KeyValuePair<ParameterExpression, MethodCallExpression>(parameterExpression, compareToCall);
+        }
+
+        private static MethodInfo GetCompareToMethodInfo<TProperty>()
+        {
+            var type = typeof (TProperty);
+            var methods = type.GetMethods().Where(x => x.Name == "CompareTo").ToArray();
+            if (methods.Length == 1)
+                return methods[0];
+            foreach (var info in methods)
+            {
+                var infoType = info.GetParameters().Select(x => x.ParameterType).Single();
+                if (infoType == type)
+                    return info;
+            }
+            throw new Exception("Never throws");
+        }
+
+        public static CustomComparer<T> New<TProperty>(Expression<Func<T, TProperty>> member) where TProperty : IComparable<TProperty>, IComparable
+        {
+            var comparator = new CustomComparer<T>();
+            var body = CreateBody(comparator, member, false);
+            comparator.Value = body;
+            return comparator;
+        }
+
+        public CustomComparer<T> Add<TProperty>(Expression<Func<T, TProperty>> member) where TProperty : IComparable<TProperty>, IComparable
+        {
+            var additionalComparasion = CreateBody(this, member, false);
+            var visitor = new CustomComparerVisitor(additionalComparasion);
+            Value = (BlockExpression) visitor.Visit(Value);
+            return this;
+        }
+
+        public static CustomComparer<T> NewNeg<TProperty>(Expression<Func<T, TProperty>> member) where TProperty : IComparable<TProperty>, IComparable
+        {
+            var comparator = new CustomComparer<T>();
+            var body = CreateBody(comparator, member, true);
+            comparator.Value = body;
+            return comparator;
+        }
+
+        public CustomComparer<T> AddNeg<TProperty>(Expression<Func<T, TProperty>> member) where TProperty : IComparable<TProperty>, IComparable
+        {
+            var additionalComparasion = CreateBody(this, member, true);
+            var visitor = new CustomComparerVisitor(additionalComparasion);
+            Value = (BlockExpression) visitor.Visit(Value);
+            return this;
+        }
+
+        public static explicit operator Expression<Comparison<T>>(CustomComparer<T> obj)
+        {
+            return Expression.Lambda<Comparison<T>>(obj.Value, Parameters);
+        }
+
+        public Comparison<T> CreateDelegate()
+        {
+            ConstantExpression zero = Expression.Constant(0, typeof (int));
+            Value = Expression.Block(Value, Expression.Label(RetLabelTarget, zero));
+            var result = ((Expression<Comparison<T>>) this);
+            return result.Compile();
+        }
+
+        public IComparer<T> CreateComparer()
+        {
+            return Comparer<T>.Create(CreateDelegate());
+        }
+    }
+}
