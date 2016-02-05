087b20009715d18f033b6d292caa522a0131979c
Pzixel <pzixel@gmail.com>
2016-02-05 14:40:58 +0300

    Unused dlls removed

---
+++ ZLinq.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLinq.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = ZEnumerable.Range(1, 128*1024*1024);
+
+            double res = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                res += arr.Max();
+            }
+            System.Console.WriteLine(res);
+        }
+    }
+}
