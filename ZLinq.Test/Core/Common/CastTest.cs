99a4f4b3ff82f084c75920213a321801443b7a2f
Pzixel <pzixel@gmail.com>
2016-02-19 14:13:34 +0300

    Cast added

---
+++ ZLinq.Test/Core/Common/CastTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZLinq.Test.Core.Unfold
+{
+    [TestClass]
+    public class CastTest
+    {
+        [TestMethod]
+        public void CastEnum()
+        {
+            var source = Enumerable.Range(1, 100).ToArray();
+            var zsource = ZEnumerable.Range(1, 100);
+
+            Assert.IsTrue(source.Length == zsource.Length);
+            Assert.IsTrue(source.SequenceEqual(zsource));
+        }
+    }
+}
---
+++ ZLinq/Core/Common/Cast.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using ZLinq.Class;
+using ZLinq.CommonInternal;
+
+// ReSharper disable once CheckNamespace
+namespace ZLinq
+{
+    public static partial class ZEnumerable
+    {
+        public static IList<T> Cast<T>(this IList source)
+        {
+            source.IsNotNull(nameof(source));
+            return CastInternal<T>(source);
+        }
+
+        private static IList<T> CastInternal<T>(IList source)
+        {
+            var ilist = source as IList<T>;
+            if (ilist != null)
+                return ilist;
+            if (source.Count == 0)
+                return new List<T>(0);
+            return new ListCaster<T>(source);
+        }
+
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static IEnumerable<T> CastSeq<T>(this IEnumerable source)
+        {
+            source.IsNotNull(nameof(source));
+            var ilist = source as IList;
+            if (ilist != null)
+            {
+                return CastInternal<T>(ilist);
+            }
+            return CastSeqInternal<T>(source);
+        }
+
+        [SuppressMessage("ReSharper", "LoopCanBeConvertedToQuery")]
+        private static IEnumerable<T> CastSeqInternal<T>(IEnumerable source)
+        {
+            foreach (T value in source)
+            {
+                yield return value;
+            }
+        }
+    }
+}
